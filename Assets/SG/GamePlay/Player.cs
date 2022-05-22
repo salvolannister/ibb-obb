793d8a8071326ceb047a9d78a0cc9f46f9ca965b
salvolannister <darksawyer@hotmail.it>
2022-05-22 08:50:45 +0100

    improvements on gravity functionality

--- Assets/SG/GamePlay/Player.cs
+++ Assets/SG/GamePlay/Player.cs
@@ -33,6 +33,7 @@ public class Player : MonoBehaviour
     private bool isJumping = false;
     private Coroutine co;
     private bool reversed = false;
+    private Vector3 lastDir = Vector3.zero;
     public float gravityChangeDelay = 0.05f;
     public void ReverseGravity()
     {
@@ -49,20 +50,26 @@ public class Player : MonoBehaviour
     private IEnumerator _ReverseGravity()
     {
         reversed = !reversed;
+       
+       // RotatePlayer();
+
+        yield return new WaitForSeconds(gravityChangeDelay);
+        gravity *= -1;
+    }
+
+    private void RotatePlayer()
+    {
         int gravityDir = reversed ? 1 : -1;
-        if (rb.velocity.x > 0)
+        if (lastDir == Vector3.right)
         { // Moving Right
             transform.rotation = Quaternion.LookRotation(
-                (reversed) ? new Vector3(0, 0, -1.0f) : new Vector3(0, 0, 1.0f), -Vector3.up *gravityDir);
+                (reversed) ? new Vector3(0, 0, -1.0f) : new Vector3(0, 0, 1.0f), -Vector3.up * gravityDir);
         }
-        else if (rb.velocity.x < 0)
+        else if (lastDir == Vector3.left)
         { // Moving Left
             transform.rotation = Quaternion.LookRotation(
                 (reversed) ? new Vector3(0, 0, 1.0f) : new Vector3(0, 0, -1.0f), -Vector3.up * gravityDir);
         }
-
-        yield return new WaitForSeconds(gravityChangeDelay);
-        gravity *= -1;
     }
 
     private Vector3 vectorVelocity;
@@ -143,10 +150,17 @@ public class Player : MonoBehaviour
     private void Translate(Vector3 direction)
     {
         //if (Time.realtimeSinceStartup - timeRead > (yMov < 0 ? fallTime / 10 : fallTime))
+        lastDir = direction;
         transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
+       // RotatePlayer();
         transform.transform.Translate(direction * moveSpeed * Time.deltaTime, 0);
     }
 
+    void OnAnimatorMove()
+    {
+        RotatePlayer();
+    }
+   
     public void ChangeGravity()
     {
         gravity = -gravity;

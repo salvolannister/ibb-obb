9fd3348aa5d03e25867af24aa636f4bfa2158d90
salvolannister <darksawyer@hotmail.it>
2022-05-24 19:02:01 +0100

    Added Player Settings in another class

---
+++ Assets/SG/GamePlay/PlayerSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Assets.SG.GamePlay
+{
+    public static class PlayerSettings
+    {
+        public const float Y_VELOCITY_LIMIT = 5f;
+        public const float VEL_DECELERATION_FACTOR = 0.3f;
+        public const float FORCE_DECELERATION_FACTOR = 0.2f;
+    }
+
+}
